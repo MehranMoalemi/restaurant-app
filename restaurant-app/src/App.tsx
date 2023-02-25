c733b08bbf749ba2f9446cffad9fb89eb43bd2dd
MehranMoalemi <mohammadmoallemi78@gmail.com>
2023-02-25 05:38:55 +0330

    ScrollINtoView Function

--- restaurant-app/src/App.tsx
+++ restaurant-app/src/App.tsx
@@ -3,31 +3,30 @@ import { FaPizzaSlice } from 'react-icons/fa';
 import { FaHamburger } from 'react-icons/fa';
 import { FaAppleAlt } from 'react-icons/fa';
 import CategorySelector from './components/CategorySelector';
-import { useRef } from 'react';
+import { RefObject, useRef } from 'react';
 import CategoriesContainer from './components/CategorySection';
 function App() {
   const pizzaRef = useRef<HTMLDivElement>(null)
   const burgerRef = useRef<HTMLDivElement>(null)
   const fruitRef = useRef<HTMLDivElement>(null)
-  const handleClick = () => {
-    if (pizzaRef.current) {
-      pizzaRef.current.scrollIntoView({ behavior: "smooth" });
+  
+  const handleClick = (ref: RefObject<HTMLDivElement>) => {
+    if (ref.current) {
+      ref.current.scrollIntoView({ behavior: "smooth" });
     }
   };
   return (
     <div>
       <div className='categories-container'>
-        <CategorySelector onClick={()=>pizzaRef.current?.click()} Icon={FaPizzaSlice} title={'pizza'} />
-        <CategorySelector onClick={()=>burgerRef.current?.click()} Icon={FaHamburger} title={'burger'} />
-        <CategorySelector onClick={()=>fruitRef.current?.click()} Icon={FaAppleAlt} title={'fruit'} />
+        <CategorySelector onClick={()=>handleClick(pizzaRef)} Icon={FaPizzaSlice} title={'pizza'} />
+        <CategorySelector onClick={()=>handleClick(burgerRef)} Icon={FaHamburger} title={'burger'} />
+        <CategorySelector onClick={()=>handleClick(fruitRef)} Icon={FaAppleAlt} title={'fruit'} />
       </div>
       <div className='sections-container'>
         <CategoriesContainer Icon={FaPizzaSlice} title={'pizza'} forwardedRef={pizzaRef} />
         <CategoriesContainer Icon={FaHamburger} title={'burger'} forwardedRef={burgerRef} />
         <CategoriesContainer Icon={FaAppleAlt} title={'fruits'} forwardedRef={fruitRef} />
       </div>
-
-
     </div>
   );
 }
