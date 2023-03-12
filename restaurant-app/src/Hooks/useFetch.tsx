c91593ae7b7918651410eec981409cc88062b4ef
MehranMoalemi <mohammadmoallemi78@gmail.com>
2023-03-12 07:29:08 +0330

    useEffectAfterMount added to useFetch

--- restaurant-app/src/Hooks/useFetch.tsx
+++ restaurant-app/src/Hooks/useFetch.tsx
@@ -1,39 +1,61 @@
 import { useState, useEffect } from "react";
+import useEffectAfterMount from "./useEffectAfterMount";
 
 interface useFetchReturns<T> {
   data: T | any;
   error: Error | null;
 }
 
-interface Props{
+interface Props {
   url: string;
   dependencies: any[];
+  fetchOnFirstRender: boolean;
 }
 
 function useFetch<T>(props: Props): useFetchReturns<T> {
-  const { url, dependencies=[] } = props;
+  const { url, dependencies = [], fetchOnFirstRender } = props;
   console.log("called")
 
   const [data, setData] = useState<T | undefined>();
   const [error, setError] = useState<any>(null);
-  useEffect(() => {
+  if (fetchOnFirstRender) {
+    useEffect(() => {
 
-    const getData = async () => {
-      try {
-        const response = await fetch(
-          `${url}`
-        );
-        const data = await response.json();
-        setData(data);
-      } catch (error) {
-        console.log(error);
-        setError(error)
+      const getData = async () => {
+        try {
+          const response = await fetch(
+            `${url}`
+          );
+          const data = await response.json();
+          setData(data);
+        } catch (error) {
+          console.log(error);
+          setError(error)
+        }
       }
-    }
 
-    url && getData();
+      url && getData();
 
-  }, dependencies)
+    }, dependencies)
+  } else {
+    //this hook will ignore first render and doesnt fetch the data
+    useEffectAfterMount(() => {
+      const getData = async () => {
+        try {
+          const response = await fetch(
+            `${url}`
+          );
+          const data = await response.json();
+          setData(data);
+        } catch (error) {
+          console.log(error);
+          setError(error)
+        }
+      }
+
+      url && getData();
+    }, dependencies)
+  }
 
   return {
     data, error
